cae79a993c7b04819f43913c97eb5d627b957bdf
Shaxinjon2007 <shaxinjonn@gmail.com>
2023-04-30 21:00:00 +0500

    7

--- List07.py
+++ List07.py
@@ -6,4 +6,10 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    b=0
+    while b<len(list1):
+        if list1[b]==0:
+            list1[b]="False"
+        b=b+1
+    return list1
+print(main([9,0,0,9,0]))
